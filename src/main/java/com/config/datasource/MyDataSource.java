5b24c30d599b343489a250a1ebe76ea1464ade0a
code2zp <847587146@qq.com>
2021-06-08 22:14:12 +0800

    连接池改为drid

--- src/main/java/com/config/datasource/MyDataSource.java
+++ src/main/java/com/config/datasource/MyDataSource.java
@@ -1,5 +1,6 @@
 package com.config.datasource;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
@@ -12,13 +13,15 @@ public class MyDataSource {
     @Bean(name = "bigDataSource")
     @ConfigurationProperties(prefix = "spring.datasource.bigdata")
     public DataSource dataSourceBigData() {
-        return DataSourceBuilder.create().build();
+//        return DataSourceBuilder.create().build();
+        return new DruidDataSource();
     }
     //datasource
     @Bean(name = "forumDataSource")
     @ConfigurationProperties(prefix = "spring.datasource.forum")
     public DataSource dataSourceForum() {
-        return DataSourceBuilder.create().build();
+//        return DataSourceBuilder.create().build();
+        return new DruidDataSource();
     }
 
 
