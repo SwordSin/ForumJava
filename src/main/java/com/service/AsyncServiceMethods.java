9f16b859a1b266dbef5555971fa3f2f429ade070
zzp <847587146@qq.com>
2021-12-15 23:16:35 +0800

    获取用户的真实ip

--- src/main/java/com/service/AsyncServiceMethods.java
+++ src/main/java/com/service/AsyncServiceMethods.java
@@ -5,6 +5,7 @@ import com.common.RedisCommon;
 import com.dao.forum.LoginLogMapper;
 import com.dao.pojo.login.LoginLogDO;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +20,9 @@ public class AsyncServiceMethods {
     private LoginLogMapper loginLogMapper;
     @Resource
     private RedisCommon<LoginLogDO> redisCommonLoginDataDo;
+    @Value(value = "${realIpKey}")
+    private String realIpKey;
+
 
     @Async("scorePoolTaskExecutor")
     public Integer saveLoginLog(String username, Boolean rememberMe, HttpServletRequest req) {
@@ -26,7 +30,7 @@ public class AsyncServiceMethods {
         LoginLogDO loginLogDO = new LoginLogDO();
         loginLogDO.setUsername(username);
         loginLogDO.setRememberMe(rememberMe);
-        loginLogDO.setIp(req.getRemoteAddr());
+        loginLogDO.setIp(req.getHeader(realIpKey));
         loginLogDO.setLoginDate(new Date());
         Integer saveStatus = loginLogMapper.insert(loginLogDO);
         // 判断是否保存成功
